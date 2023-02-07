659b944c2e59b7813d44a947930c365d3fc01b6a
ChicMic Technologies <chicmictechnologies@ChicMics-Mac-mini.local>
2023-02-07 13:09:46 +0530

    Added dynamic Json data loading in scrollview

--- assets/scripts/scrollView_Script.ts
+++ assets/scripts/scrollView_Script.ts
@@ -1,98 +1,51 @@
-import { _decorator, Component, Node, ScrollView, Prefab, instantiate, Label } from "cc";
+import {
+  _decorator,
+  Component,
+  Node,
+  ScrollView,
+  Prefab,
+  instantiate,
+  pipeline,
+  Label,
+  loader,
+  JsonAsset,
+} from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("scrollView_Script")
 export class scrollView_Script extends Component {
+  @property(Prefab)
+  myFab: Prefab | null = null;
 
-    @property(Prefab)
-    myFab: Prefab | null = null;
+  @property(JsonAsset)
+  jsonFile: JsonAsset | null = null;
 
-    data = [
-        {
-            "name": "Anish Kumar",
-            "rank": "1",
-            "stars": "⭐️⭐️⭐️⭐️"
-        },
-        {
-            "name": "Rahul Gupta",
-            "rank": "1",
-            "stars": "⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "2",
-            "stars": "⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "3",
-            "stars": "⭐️⭐️"
-        },
-        {
-            "name": "Manish Kumar",
-            "rank": "4",
-            "stars": "⭐️⭐️⭐️"
-        },
-        {
-            "name": "Manipuri",
-            "rank": "5",
-            "stars": "⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "6",
-            "stars": "⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "7",
-            "stars": "⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "9",
-            "stars": "⭐️⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "10",
-            "stars": "⭐️⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "12",
-            "stars": "⭐️⭐️⭐️⭐️"
-        },
-        {
-            "name": "Anish Kumar",
-            "rank": "13",
-            "stars": "⭐️⭐️⭐️⭐️"
-        },
-        {
-            "name": "Sohan Kumar",
-            "rank": "14",
-            "stars": "⭐️⭐️⭐️⭐️"
-        }
-    ];
+  start() {
+    console.log("Loading Data....");
+    this.loadData();
 
-    start() { 
-        console.log("Loading Data....");
-        this.loadData();
-    }
+    // var url = cc.url.raw( '/usersData.json' )
+    // loader.loadRes( "/usersData.json", function( err, res)
+    // {
+    //     console.log( 'load['+ "/path" +'], err['+err+'] result: ' + JSON.stringify( res ) );
+    // });
 
-    update(deltaTime: number) { }
+    console.log("json file", this.jsonFile?.json?.data);
+  }
 
-    loadData(){
-        this.data.forEach(person => {
+  update(deltaTime: number) {}
 
-            let item = instantiate(this.myFab);
+  loadData() {
+    let personData = this.jsonFile?.json?.data;
 
-            item.getChildByName('name').getComponent(Label).string = person.name;
-            item.getChildByName('rank').getComponent(Label).string = person.rank;
-            item.getChildByName('stars').getComponent(Label).string = person.stars;
-            
-            this.node.getComponent(ScrollView).content.addChild(item);
+    personData.forEach((person) => {
+      let item = instantiate(this.myFab);
 
-        });
-    }
+      item.getChildByName("name").getComponent(Label).string = person.name;
+      item.getChildByName("rank").getComponent(Label).string = person.rank;
+      item.getChildByName("stars").getComponent(Label).string = person.stars;
+
+      this.node.getComponent(ScrollView).content.addChild(item);
+    });
+  }
 }
